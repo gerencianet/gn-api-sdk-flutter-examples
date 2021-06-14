c839682ee43c8130f3bb4f8bc10015a0605ade9d
Igor Pedroso <igor.pedroso@gerencianet.com.br>
2021-06-14 16:52:57 -0300

    create screen create charge history

---
+++ lib/screens/default/charge/create_charge_history.dart
@@ -0,0 +1,210 @@
+import 'package:examplesgn/components/form.dart';
+import 'package:examplesgn/credentials.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gerencianet/gerencianet.dart';
+
+class CreateChargeHistory extends StatefulWidget {
+  @override
+  _CreateChargeHistoryState createState() => _CreateChargeHistoryState();
+}
+
+class _CreateChargeHistoryState extends State<CreateChargeHistory> {
+  TextEditingController _idController = new TextEditingController();
+  TextEditingController _textController = new TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Gerencianet gn;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Map credentials = new Map.from(CREDENTIALS);
+    credentials.remove("pix_cert");
+    credentials.remove("pix_private_key");
+    gn = new Gerencianet(credentials);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: _appBar(),
+        bottomSheet: _bottomSheet(),
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: _body());
+  }
+
+  Widget _body() {
+    return SingleChildScrollView(
+        child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                _headerForm("Transação",
+                    "O campo ID é referente ao ID de uma transação criada anteriormente."),
+                _form(_formId()),
+                _headerForm("Descrição",
+                    "O campo abaixo é referente ao texto a ser acrescentado no histórico da transação."),
+                _form(_formText()),
+              ],
+            )));
+  }
+
+  _headerForm(String title, String content) {
+    return Container(
+      margin: EdgeInsets.only(top: 15, right: 5, left: 5),
+      color: Colors.white,
+      padding: EdgeInsets.only(left: 15, right: 15, top: 15),
+      child: Row(
+        children: [
+          Expanded(
+              child: Text(title,
+                  style: TextStyle(color: Theme.of(context).primaryColor))),
+          _buttonInfo(title, content, Color(0xFF00b4c5))
+        ],
+      ),
+    );
+  }
+
+  Widget _form(Widget form) {
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 5),
+        color: Colors.white,
+        padding: EdgeInsets.only(left: 15, right: 15, bottom: 50, top: 15),
+        child: form);
+  }
+
+  Widget _formText() {
+    return FormDataField(
+      helperText: "Texto a ser acrescentado ao histórico",
+      label: "Texto*",
+      line: 3,
+      controller: _textController,
+      textInputType: TextInputType.text,
+      validator: (value) => value.isEmpty ? "Campo Obrigatório!" : null,
+    );
+  }
+
+  Widget _formId() {
+    return FormDataField(
+      helperText: "Id da transação",
+      label: "Id*",
+      line: 1,
+      controller: _idController,
+      textInputType: TextInputType.number,
+      validator: (value) => value.isEmpty ? "Campo Obrigatório!" : null,
+    );
+  }
+
+  Widget _buttonInfo(String title, String content, Color color) {
+    return IconButton(
+        icon: Icon(Icons.help, color: color),
+        onPressed: () {
+          _showInfo(title, content);
+        });
+  }
+
+  void _showInfo(String title, String content) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Theme.of(context).backgroundColor,
+            title: new Text(title),
+            content: Text(content +
+                "\n\nPara mais informações acesse a nossa documentação: dev.gerencianet.com.br"),
+            contentTextStyle: TextStyle(color: Color(0xFF848484)),
+            titleTextStyle:
+                TextStyle(color: Theme.of(context).primaryColor, fontSize: 18),
+            actions: <Widget>[
+              new TextButton(
+                child: new Text(
+                  "Fechar",
+                  style: TextStyle(color: Theme.of(context).accentColor),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  Widget _bottomSheet() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: TextButton(
+        onPressed: () {
+          if (!this._loading) {
+            FocusScope.of(context).requestFocus(new FocusNode());
+            final FormState form = _formKey.currentState;
+            if (form.validate() && !_loading) {
+              _createChargeHistory();
+            } else {
+              _showMessage("Preencha os campos obrigatórios!", Colors.red, 25);
+            }
+          }
+        },
+        child: _loading
+            ? CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              )
+            : Text(
+                "ACRESCENTAR",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+      ),
+    );
+  }
+
+  void _showMessage(String msg, Color color, double height) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: color,
+      content: Container(
+        height: height,
+        child: Center(
+          child: Text(msg),
+        ),
+      ),
+    ));
+  }
+
+  Widget _appBar() {
+    return AppBar(
+      elevation: 0,
+      title: Text("Acrescentar Histórico"),
+      centerTitle: true,
+      actions: [
+        _buttonInfo(
+            "Acrescentar Histórico",
+            "Acrescentar descrição ao histórico de uma transação.\n\n",
+            Colors.white)
+      ],
+    );
+  }
+
+  void _createChargeHistory() {
+    setState(() => _loading = true);
+
+    Map<String, dynamic> params = {
+      "id": _idController.text,
+    };
+
+    Map<String, dynamic> body = {
+      "description": _textController.text,
+    };
+
+    gn.call("createChargeHistory", params: params, body: body).then((value) {
+      setState(() => _loading = false);
+      _showMessage("Descrição Acrescentada!", Theme.of(context).accentColor,
+          MediaQuery.of(context).size.height);
+    }).catchError((error) {
+      setState(() => _loading = false);
+      _showMessage(
+          error.toString(), Colors.red, MediaQuery.of(context).size.height);
+    });
+  }
+}
