d31a9c284f3f2712807a165eb11dd586038f7db0
Igor Pedroso <igor.pedroso@gerencianet.com.br>
2021-06-14 16:51:33 -0300

    create screen cancel charge

---
+++ lib/screens/default/charge/cancel_charge.dart
@@ -0,0 +1,164 @@
+import 'package:examplesgn/components/form.dart';
+import 'package:examplesgn/credentials.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:gerencianet/gerencianet.dart';
+
+class CancelCharge extends StatefulWidget {
+  @override
+  _CancelChargeState createState() => _CancelChargeState();
+}
+
+class _CancelChargeState extends State<CancelCharge> {
+  TextEditingController _idController = new TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  Gerencianet gn;
+  bool _loading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Map credentials = new Map.from(CREDENTIALS);
+    credentials.remove("pix_cert");
+    credentials.remove("pix_private_key");
+    gn = new Gerencianet(credentials);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: _appBar(),
+        bottomSheet: _bottomSheet(),
+        backgroundColor: Theme.of(context).backgroundColor,
+        body: _body());
+  }
+
+  Widget _body() {
+    return SingleChildScrollView(
+        child: Form(key: _formKey, child: _form(_formId())));
+  }
+
+  Widget _form(Widget form) {
+    return Container(
+        margin: EdgeInsets.symmetric(horizontal: 5, vertical: 15),
+        color: Colors.white,
+        padding: EdgeInsets.only(left: 15, right: 15, bottom: 50, top: 15),
+        child: form);
+  }
+
+  Widget _formId() {
+    return FormDataField(
+      helperText: "Id da cobrança",
+      label: "Id*",
+      line: 1,
+      controller: _idController,
+      textInputType: TextInputType.text,
+      validator: (value) => value.isEmpty ? "Campo Obrigatório!" : null,
+    );
+  }
+
+  Widget _buttonInfo(String title, String content, Color color) {
+    return IconButton(
+        icon: Icon(Icons.help, color: color),
+        onPressed: () {
+          _showInfo(title, content);
+        });
+  }
+
+  void _showInfo(String title, String content) {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            backgroundColor: Theme.of(context).backgroundColor,
+            title: new Text(title),
+            content: Text(content +
+                "\n\nPara mais informações acesse a nossa documentação: dev.gerencianet.com.br"),
+            contentTextStyle: TextStyle(color: Color(0xFF848484)),
+            titleTextStyle:
+                TextStyle(color: Theme.of(context).primaryColor, fontSize: 18),
+            actions: <Widget>[
+              new TextButton(
+                child: new Text(
+                  "Fechar",
+                  style: TextStyle(color: Theme.of(context).accentColor),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  Widget _bottomSheet() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 50,
+      child: TextButton(
+        onPressed: () {
+          if (!this._loading) {
+            FocusScope.of(context).requestFocus(new FocusNode());
+            final FormState form = _formKey.currentState;
+            if (form.validate() && !_loading) {
+              _cancelCharge();
+            } else {
+              _showMessage("Preencha os campos obrigatórios!", Colors.red, 25);
+            }
+          }
+        },
+        child: _loading
+            ? CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+              )
+            : Text(
+                "CANCELAR",
+                style:
+                    TextStyle(color: Colors.white, fontWeight: FontWeight.w600),
+              ),
+      ),
+    );
+  }
+
+  void _showMessage(String msg, Color color, double height) {
+    ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+      backgroundColor: color,
+      content: Container(
+        height: height,
+        child: Center(
+          child: Text(msg),
+        ),
+      ),
+    ));
+  }
+
+  Widget _appBar() {
+    return AppBar(
+      elevation: 0,
+      title: Text("Cancelar Cobrança"),
+      centerTitle: true,
+      actions: [
+        _buttonInfo("Cancelar Cobrança",
+            "Cancelar uma transação existente.\n\n", Colors.white)
+      ],
+    );
+  }
+
+  void _cancelCharge() {
+    setState(() => _loading = true);
+
+    Map<String, dynamic> params = {
+      "id": _idController.text,
+    };
+    gn.call("cancelCharge", params: params).then((value) {
+      setState(() => _loading = false);
+      _showMessage("Cobrança Cancelada!", Theme.of(context).accentColor,
+          MediaQuery.of(context).size.height);
+    }).catchError((error) {
+      setState(() => _loading = false);
+      _showMessage(
+          error.toString(), Colors.red, MediaQuery.of(context).size.height);
+    });
+  }
+}
